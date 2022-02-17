1c9ebc91ef63ef23f14f889faaa8042704ea92c0
Daffarr <77198942+Daffarr@users.noreply.github.com>
2022-02-17 16:06:50 +0700

    stringJoiner Class

--- src/daffa/belajarjava/application/StringJoinerApp.java
+++ src/daffa/belajarjava/application/StringJoinerApp.java
@@ -1,8 +1,18 @@
 package daffa.belajarjava.application;
 
+import java.util.StringJoiner;
+
 public class StringJoinerApp {
     public static void main(String[] args) {
 
+        StringJoiner joiner = new StringJoiner(", ", "[", "]");
+
+        joiner.add("Daffa");
+        joiner.add("Rayhan");
+        joiner.add("Riadi");
+
+        String value = joiner.toString();
+        System.out.println(value);
 
 
     }
